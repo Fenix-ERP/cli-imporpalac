303b8de159a8cb83cc1d258fd649d90ca75bdc39
aasimbana <aasimbana@fenixerp.com>
2025-07-14 15:11:53 -0500

    [FIX] additional_electronic_voucher: Fix additional product imporpalac add new process and button update cost

--- additional_product_imporpalac/models/account_voucher_update.py
+++ additional_product_imporpalac/models/account_voucher_update.py
@@ -1,10 +1,10 @@
-from odoo import _, fields, models
+from odoo import _, models
 from odoo.exceptions import UserError
+
 
 class ElectronicVoucherUpdate(models.Model):
     _inherit = "account.evoucher"
     _description = "Electronic Voucher"
-
 
     def update_costs_action(self):
         """Update costs in the linked purchase order after validations"""
@@ -12,14 +12,22 @@ class ElectronicVoucherUpdate(models.Model):
 
         if not self.purchase_order_id:
             raise UserError(_("This e-voucher is not linked to any purchase order."))
-        
+
         if self.vendor_id != self.purchase_order_id.partner_id:
-            raise UserError(_(
-                "The vendor %s doesn't match the purchase order's vendor %s."
-            ) % (self.vendor_id.name, self.purchase_order_id.partner_id.name))
-        
-        if self.purchase_order_id.picking_ids.filtered(lambda p: p.state == 'done'):
-            raise UserError(_("You have already validated the receipt of a purchase order."))
+            raise UserError(
+                _(
+                    "The vendor %(vendor_name)s doesn't match the purchase order's vendor %(po_vendor_name)s."
+                )
+                % {
+                    "vendor_name": self.vendor_id.name,
+                    "po_vendor_name": self.purchase_order_id.partner_id.name,
+                }
+            )
+
+        if self.purchase_order_id.picking_ids.filtered(lambda p: p.state == "done"):
+            raise UserError(
+                _("You have already validated the receipt of a purchase order.")
+            )
 
         non_homologated_products = []
         for line in self.lines_ride:
@@ -29,32 +37,39 @@ class ElectronicVoucherUpdate(models.Model):
 
         if non_homologated_products:
             products_list = "\n- ".join(non_homologated_products)
-            raise UserError(_(
-                "The following products are not approved in the system:\n- %s\n"
-                "Please homologate products before updating costs."
-            ) % products_list)
+            raise UserError(
+                _(
+                    "The following products are not approved in the system:\n- %s\n"
+                    "Please homologate products before updating costs."
+                )
+                % products_list
+            )
 
         updated_lines = 0
         for line in self.lines_ride:
             if line.product_id:
                 po_line = self.purchase_order_id.order_line.filtered(
-                    lambda l: l.product_id == line.product_id
+                    lambda line: line.product_id == line.product_id
                 )
                 if po_line:
-                    po_line.write({
-                        'price_unit': line.price_unit,
-                        'discount': line.discount,
-                        'product_qty': line.quantity,
-                    })
+                    po_line.write(
+                        {
+                            "price_unit": line.price_unit,
+                            "discount": line.discount,
+                            "product_qty": line.quantity,
+                        }
+                    )
                     updated_lines += 1
 
         if not updated_lines:
-            raise UserError(_("No matching products found in the purchase order to update."))
+            raise UserError(
+                _("No matching products found in the purchase order to update.")
+            )
 
         return {
-            'effect': {
-                'fadeout': 'slow',
-                'message': _('Costs updated successfully!'),
-                'type': 'rainbow_man',
+            "effect": {
+                "fadeout": "slow",
+                "message": _("Costs updated successfully!"),
+                "type": "rainbow_man",
             }
         }
