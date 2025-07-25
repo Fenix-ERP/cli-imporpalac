862876849a181aa0b668e399e596e3089b35803e
jaarmas <programadorerp2@fenixcorp.net>
2025-07-25 12:52:16 -0500

    [FIX]cli-imporpalac: fix cli

--- sale_user_discount_limit/models/res_users.py
+++ sale_user_discount_limit/models/res_users.py
@@ -1,7 +1,8 @@
-from odoo import models, fields
+from odoo import fields, models
+
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     limit_discount_enabled = fields.Boolean(string="Limitar descuento máximo")
     max_discount_percent = fields.Float(string="Descuento máximo (%)", default=0.0)
--- sale_user_discount_limit/models/sale_order.py
+++ sale_user_discount_limit/models/sale_order.py
@@ -1,12 +1,14 @@
-from odoo import models, fields, api, exceptions, _
+from odoo import _, api, exceptions, models
+
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    @api.onchange('discount')
+    @api.onchange("discount")
     def _check_discount_limit(self):
         user = self.env.user
         if user.limit_discount_enabled and self.discount > user.max_discount_percent:
-            raise exceptions.UserError(_(
-                "No puedes aplicar un descuento mayor al %.2f%%." % user.max_discount_percent
-            ))
+            raise exceptions.UserError(
+                _("No puedes aplicar un descuento mayor al %.2f%%.")
+                % user.max_discount_percent
+            )
