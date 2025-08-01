c3db911dd5e123dcea82cafae224f735745073fe
Jorge Armas <programadorerp2@fenixcorp.net>
2025-08-01 09:35:07 -0500

    [FIX]sale_order_payment: fix company domain (#35)
    * [FIX]sale_order_payment: fix company domain
    
    * [FIX]sale_order_payment: fix company domain

--- sale_order_payment/__manifest__.py
+++ sale_order_payment/__manifest__.py
@@ -6,6 +6,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/sale_order_payment_views.xml",
+        "security/sale_order_payment_security.xml",
         "views/sale_order_inherit.xml",
         "views/stock_picking.xml",
         "wizards/sale_order_payment_method_wizard.xml",
