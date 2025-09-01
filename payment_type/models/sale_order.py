f81eb917710d30b94ffee509e5430e9469d2dae2
Jorge Armas <programadorerp2@fenixcorp.net>
2025-09-01 15:47:13 -0500

    [FIX]cli-imporpalac: fix cli_imporpalac (#56)

--- payment_type/models/sale_order.py
+++ payment_type/models/sale_order.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -8,16 +9,29 @@ class SaleOrder(models.Model):
         comodel_name="payment.type",
         string="Payment method",
         ondelete="cascade",
-        readonly=True,
+        default=lambda self: self.env.ref(
+            "payment_type.payment_type_cash", raise_if_not_found=False
+        ),
     )
 
     credit = fields.Monetary(
         related="partner_id.credit",
     )
 
+    @api.onchange("partner_id")
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.payment_method = self.partner_id.payment_method
+
     def action_confirm(self):
         res = super().action_confirm()
         for order in self:
+            identifier = order.partner_id.is_end_consumer
+            if identifier:
+                if order.amount_total > order.company_id.max_amount_final_consumer:
+                    raise ValidationError(
+                        _("Exceeds the maximum value for end consumers")
+                    )
             pickings = order.picking_ids.filtered(
                 lambda p: p.state not in ["done", "cancel"]
             )
@@ -26,14 +40,9 @@ class SaleOrder(models.Model):
         return res
 
     def _create_invoices(self, grouped=False, final=False):
-        """
-        Sobrescritura del método para asignar automáticamente el diario en la factura
-        dependiendo del almacén de la entrega.
-        """
         invoices = super(SaleOrder, self)._create_invoices(grouped=grouped, final=final)
 
         for invoice in invoices:
-            # Obtener el picking relacionado con la orden
             related_pickings = self.picking_ids.filtered(lambda p: p.state == "done")
             if related_pickings:
                 warehouse = related_pickings[0].picking_type_id.warehouse_id
