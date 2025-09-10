3e12194fbbdb66df0b5b1c7e615556ba92d17b0d
jaarmas <programadorerp2@fenixcorp.net>
2025-09-10 17:41:55 -0500

    [FIX]cli-imporpalac: fix pricelist

--- price_list_line/models/sale_order_line.py
+++ price_list_line/models/sale_order_line.py
@@ -1,32 +1,26 @@
-from odoo import api, fields, models
-
+from odoo import models, fields, api
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     line_pricelist_id = fields.Many2one(
-        "product.pricelist", string="Lista de Precios por Línea"
+        'product.pricelist',
+        string="Lista de Precios por Línea"
     )
 
-    @api.onchange("product_id", "order_id.pricelist_id", "product_uom_qty")
-    def _onchange_product_id_set_line_pricelist(self):
+    @api.onchange('product_id', 'line_pricelist_id', 'order_id.pricelist_id', 'product_uom_qty')
+    def _onchange_line_pricelist_id(self):
         for line in self:
-            if line.product_id:
-                if not line.line_pricelist_id:
-                    line.line_pricelist_id = line.order_id.pricelist_id
-
-                pricelist = line.line_pricelist_id or line.order_id.pricelist_id
+            pricelist = line.line_pricelist_id or line.order_id.pricelist_id
+            if line.product_id and pricelist:
                 line.price_unit = pricelist._get_product_price(
-                    line.product_id,
-                    line.product_uom_qty or 1.0,
-                    line.order_id.partner_id,
+                    line.product_id, line.product_uom_qty or 1.0, line.order_id.partner_id
                 )
 
-
 class SaleOrder(models.Model):
-    _inherit = "sale.order"
+    _inherit = 'sale.order'
 
-    @api.onchange("pricelist_id")
+    @api.onchange('pricelist_id')
     def _onchange_pricelist_id(self):
         for order in self:
             for line in order.order_line:
