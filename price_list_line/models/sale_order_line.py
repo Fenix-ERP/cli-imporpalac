87872df1e7262f246d19aaf834090f07d6d08d9c
Jorge Armas <programadorerp2@fenixcorp.net>
2025-09-10 17:47:34 -0500

    [FIX]cli-imporpalac: fix pricelist (#69)
    * [FIX]cli-imporpalac: fix pricelist
    
    * [FIX]cli-imporpalac: fix pricelist

--- price_list_line/models/sale_order_line.py
+++ price_list_line/models/sale_order_line.py
@@ -8,14 +8,13 @@ class SaleOrderLine(models.Model):
         "product.pricelist", string="Lista de Precios por Línea"
     )
 
-    @api.onchange("product_id", "order_id.pricelist_id", "product_uom_qty")
-    def _onchange_product_id_set_line_pricelist(self):
+    @api.onchange(
+        "product_id", "line_pricelist_id", "order_id.pricelist_id", "product_uom_qty"
+    )
+    def _onchange_line_pricelist_id(self):
         for line in self:
-            if line.product_id:
-                if not line.line_pricelist_id:
-                    line.line_pricelist_id = line.order_id.pricelist_id
-
-                pricelist = line.line_pricelist_id or line.order_id.pricelist_id
+            pricelist = line.line_pricelist_id or line.order_id.pricelist_id
+            if line.product_id and pricelist:
                 line.price_unit = pricelist._get_product_price(
                     line.product_id,
                     line.product_uom_qty or 1.0,
