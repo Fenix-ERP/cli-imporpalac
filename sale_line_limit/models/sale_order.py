d2ccdc6a408117a9744032366d27497fbd5afb16
jaarmas <programadorerp2@fenixcorp.net>
2025-09-29 13:55:25 -0500

    [FIX]cli_imporpalac: fix reports

--- sale_line_limit/models/sale_order.py
+++ sale_line_limit/models/sale_order.py
@@ -23,3 +23,23 @@ class SaleOrder(models.Model):
                 _("No puedes agregar más de %s líneas de venta a este pedido.")
                 % max_lines
             )
+
+    def action_confirm(self):
+        if (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("sale_line_limit.restriction")
+            == "True"
+        ):
+            max_lines = int(
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("sale_line_limit.max_lines")
+                or 0
+            )
+            if len(self.order_line) > max_lines:
+                raise exceptions.UserError(
+                    _("No puedes agregar más de %s líneas de venta a este pedido.")
+                    % max_lines
+                )
+        return super().action_confirm()
