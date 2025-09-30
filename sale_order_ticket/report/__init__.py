68b3f60843f70b4c99372ce65279fa8f4315049e
jaarmas <programadorerp2@fenixcorp.net>
2025-09-30 11:31:30 -0500

    [FIX]cli_imporpalac: fix reports

---
+++ sale_order_ticket/report/__init__.py
@@ -0,0 +1,2 @@
+from . import report_ticket_dispatch
+from . import report_ticket_cash
---
+++ sale_order_ticket/report/report_ticket_cash.py
@@ -0,0 +1,23 @@
+from odoo import models
+from odoo.modules.module import get_module_resource
+import base64
+
+class ReportTicketCash(models.AbstractModel):
+    _name = 'report.sale_order_ticket.report_ticket_cash'
+    _description = 'Quotation Cash Ticket Report'
+
+    def _get_logo(self):
+        """Cargar logo fijo desde el módulo y convertir a base64"""
+        path = get_module_resource('sale_order_ticket', 'static/src/img/logo.png')
+        try:
+            with open(path, 'rb') as f:
+                return base64.b64encode(f.read()).decode('utf-8')
+        except Exception:
+            return False
+
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['sale.order'].browse(docids)
+        return {
+            'docs': docs,
+            'logo_custom': self._get_logo(),  # aquí pasa el logo al QWeb
+        }
