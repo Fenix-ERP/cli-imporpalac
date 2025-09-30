2cb36813e7717d125a1340f486a93702e20ab61d
jaarmas <programadorerp2@fenixcorp.net>
2025-09-30 11:38:49 -0500

    [FIX]cli_imporpalac: fix reports

--- sale_order_ticket/report/report_ticket_dispatch.py
+++ sale_order_ticket/report/report_ticket_dispatch.py
@@ -1,23 +1,25 @@
-from odoo import models
-from odoo.modules.module import get_module_resource
 import base64
 
+from odoo import models
+from odoo.modules.module import get_module_resource
+
+
 class ReportTicketDispatch(models.AbstractModel):
-    _name = 'report.sale_order_ticket.report_ticket_dispatch'
-    _description = 'Quotation Dispatch Ticket Report'
+    _name = "report.sale_order_ticket.report_ticket_dispatch"
+    _description = "Quotation Dispatch Ticket Report"
 
     def _get_logo(self):
         """Cargar logo fijo desde el módulo y convertir a base64"""
-        path = get_module_resource('sale_order_ticket', 'static/src/img/logo.png')
+        path = get_module_resource("sale_order_ticket", "static/src/img/logo.png")
         try:
-            with open(path, 'rb') as f:
-                return base64.b64encode(f.read()).decode('utf-8')
+            with open(path, "rb") as f:
+                return base64.b64encode(f.read()).decode("utf-8")
         except Exception:
             return False
 
     def _get_report_values(self, docids, data=None):
-        docs = self.env['sale.order'].browse(docids)
+        docs = self.env["sale.order"].browse(docids)
         return {
-            'docs': docs,
-            'logo_custom': self._get_logo(),  # aquí pasa el logo al QWeb
+            "docs": docs,
+            "logo_custom": self._get_logo(),  # aquí pasa el logo al QWeb
         }
