2c4b4f6e4a6b81dfe2220aab2c509dfb460f2e48
aasimbana <144495487+aasimbana@users.noreply.github.com>
2025-10-17 11:45:16 -0500

    [FIX] Add new validation assigned ubication internal in the move stock (#114)
    * [FIX] Add new validation assigned ubication internal in the move stock
    
    * [FIX] Add new validation assigned ubication internal in the move stock

--- additional_product_imporpalac/models/stock_picking_inherit.py
+++ additional_product_imporpalac/models/stock_picking_inherit.py
@@ -24,33 +24,30 @@ class StockMove(models.Model):
 
     @api.model
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
-        """
-        Esta es la mejor opción - se ejecuta cuando se crean las operaciones de stock
-        """
-        vals = super(StockMove, self)._prepare_move_line_vals(
+        vals = super()._prepare_move_line_vals(
             quantity=quantity, reserved_quant=reserved_quant
         )
 
-        # Asignar la ubicación específica de la línea de venta
-        if self.sale_line_id and self.sale_line_id.internal_location_id:
+        # Assign the location if it is an outgoing pick (sale).
+        if (
+            self.picking_id.picking_type_id.code == "outgoing"
+            and self.sale_line_id
+            and self.sale_line_id.internal_location_id
+        ):
             vals["location_id"] = self.sale_line_id.internal_location_id.id
 
         return vals
 
     def _action_assign(self):
-        """
-        Como respaldo, asegurar que las operaciones existentes tengan la ubicación correcta
-        """
-        super_result = super(StockMove, self)._action_assign()
+        super_result = super()._action_assign()
 
-        # Verificar y corregir ubicaciones después de la asignación
         for move in self:
             if (
-                move.sale_line_id
+                move.picking_id.picking_type_id.code == "outgoing"
+                and move.sale_line_id
                 and move.sale_line_id.internal_location_id
                 and move.move_line_ids
             ):
-
                 for move_line in move.move_line_ids:
                     if move_line.location_id != move.sale_line_id.internal_location_id:
                         move_line.location_id = move.sale_line_id.internal_location_id
