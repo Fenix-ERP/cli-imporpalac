0c1eef03601d24233b5d450f97ee85dd31ce1919
Daniela-Soraya-L <danielalaglacarate@gmail.com>
2025-12-25 15:42:20 -0500

    [FIX] zebra: update design of the zebra (#129)

--- zebra/wizard/picking_zebra_print_label.py
+++ zebra/wizard/picking_zebra_print_label.py
@@ -1,26 +1,21 @@
-from odoo import api, fields, models
+from odoo import _, fields, models
 
 
 class PrintLabelWizard(models.TransientModel):
     _name = "print.label.wizard"
     _description = "Wizard for printing Zebra labels"
 
-    print_barcode = fields.Boolean(string="Print barcode", default=True)
-    print_supplier_barcode = fields.Boolean(
-        string="Print supplier barcode", default=True
+    barcode_type = fields.Selection(
+        [("internal", _("Internal Barcode")), ("supplier", _("Supplier Barcode"))],
+        string="Select barcode to print",
+        default="internal",
+        required=True,
     )
 
-    @api.onchange("print_barcode", "print_supplier_barcode")
-    def _onchange_print_options(self):
-        if self.print_barcode:
-            self.print_supplier_barcode = False
-        elif self.print_supplier_barcode:
-            self.print_barcode = False
-
     def action_print_labels(self):
+        self.ensure_one()
         picking_id = self.env.context.get("active_id")
         picking = self.env["stock.picking"].browse(picking_id)
-
-        return picking.action_print_test_label(
-            self.print_barcode, self.print_supplier_barcode
-        )
+        print_barcode = self.barcode_type == "internal"
+        print_supplier_barcode = self.barcode_type == "supplier"
+        return picking.action_print_test_label(print_barcode, print_supplier_barcode)
