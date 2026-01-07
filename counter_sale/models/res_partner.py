1ed16d17ba5724d408ddff3049272638bfa33503
Daniela-Soraya-L <danielalaglacarate@gmail.com>
2026-01-07 17:15:58 -0500

    [UPD] counter_sale: add validation of contact (#130)
    * [UPD] counter_sale: add validation of contact
    
    * [UPD] counter_sale: add validation for group.

---
+++ counter_sale/models/res_partner.py
@@ -0,0 +1,21 @@
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.model
+    def write(self, vals):
+        if "identifier" in vals or "vat" in vals:
+            if not self.env.user.has_group(
+                "counter_sale.group_counter_sale_identifier_editor"
+            ):
+                raise ValidationError(
+                    _(
+                        "Editing the customer's ID is not allowed. "
+                        "Are you trying to create a new customer? "
+                        "Try using the 'New' button."
+                    )
+                )
+        return super().write(vals)
